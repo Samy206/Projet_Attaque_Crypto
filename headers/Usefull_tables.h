2a97ff3fce2f49182da441b97b39c15efa4dddb0
Clem0908 <clementlyonnet@hotmail.fr>
2021-05-05 11:43:30 +0200

    Comments on Usefull_tables.h

--- headers/Usefull_tables.h
+++ headers/Usefull_tables.h
@@ -1,9 +1,15 @@
-#include "Chiffrement/keys.h"
-
 #ifndef TABLES
 #define TABLES
 
+#include "Chiffrement/keys.h"
+
+/* Tableau représentant la S-box. */
 extern const uint4 s_box[16];
+
+/* Tableau réciproque de la S-box. */
 extern const uint4 un_s_box[16];
+
+/* Tableau repésentant la P-box. */
 extern const int p_box[24];
+
 #endif
