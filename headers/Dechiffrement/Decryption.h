b559c6cb27422c82f2260871af97d0f974888ab4
Clem0908 <clementlyonnet@hotmail.fr>
2021-05-05 11:41:09 +0200

    Comments on Decryption.h

--- headers/Dechiffrement/Decryption.h
+++ headers/Dechiffrement/Decryption.h
@@ -1,12 +1,16 @@
-#ifndef DM_CRYPTO_DECRYPTION
-#define DM_CRYPTO_DECRYPTION
+#ifndef DECRYPTION
+#define DECRYPTION
+
 #include "../Chiffrement/keys.h"
 #include "../Chiffrement/Encryption.h"
 
+/* Fonction réciproque de la fonction permutation. */
 void un_permutation(uint24 *state);
+
+/* Fonction réciproque de la fonction substitution. */
 void un_substitution(uint24 *state);
 
+/* Fonction réciproque de la fonction de chiffrement. */
 uint24 un_present(Key * key, uint24 message);
-
 
 #endif
