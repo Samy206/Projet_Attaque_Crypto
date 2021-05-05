f1ecfcb649571202a41e7927fb81728d89c4483c
Clem0908 <clementlyonnet@hotmail.fr>
2021-05-05 11:39:00 +0200

    Comments on Encryption.h

--- headers/Chiffrement/Encryption.h
+++ headers/Chiffrement/Encryption.h
@@ -1,11 +1,22 @@
 #ifndef ENCRYPTION
 #define ENCRYPTION
+
 #include "keys.h"
 
+/* Fonction de permutation,
+ * agit sur un état de 24 bits. */
 void permutation(uint24 *state);
+
+/* Fonction de substitution,
+ * agit sur un état de 24 bits. */
 void substitution(uint24 *state);
 
+/* Fonction de chiffrement PRESENT24.
+ * appelle les fonctions de permutation,
+ * de substitution. */ 
 uint24 present(Key * key, uint24 message);
+
+/* Fonction de chiffrement 2PRESENT24. */
 uint24 two_present(Key * keyA, Key * keyB, uint24 message);
 
 #endif
